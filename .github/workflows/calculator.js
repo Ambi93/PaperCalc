4a7da151c88b3f5af242858c8c140f36bb2d47f7
Ambi93 <128369830+Ambi93@users.noreply.github.com>
2023-08-14 15:54:58 +1000

    Update calculator.js

--- .github/workflows/calculator.js
+++ .github/workflows/calculator.js
@@ -11,3 +11,37 @@ document.getElementById('paperType').addEventListener('change', function() {
         document.getElementById(selectedType).style.display = 'block';
     }
 });
+
+// Event listener for the "Calculate" button
+document.getElementById('calculateButton').addEventListener('click', function() {
+    // Get the selected paper type
+    var paperType = document.getElementById('paperType').value;
+
+    // Perform calculations based on the selected paper type
+    switch (paperType) {
+        case 'standardSheets':
+            var standardSheetFormat = document.getElementById('standardSheetFormat').value;
+            var standardSheetQuantity = document.getElementById('standardSheetQuantity').value;
+            var standardSheetPrice = document.getElementById('standardSheetPrice').value;
+
+            // Calculate the area for the selected sheet size (e.g., A1)
+            var sheetArea = calculateSheetArea(standardSheetFormat);
+
+            // Calculate total area and total cost
+            var totalArea = sheetArea * standardSheetQuantity;
+            var totalCost = standardSheetPrice * standardSheetQuantity;
+
+            // Display the results
+            alert('Total Area: ' + totalArea + ', Total Cost: ' + totalCost);
+            break;
+
+        // Other cases for different paper types will go here
+    }
+});
+
+// Function to calculate sheet area based on the selected size
+function calculateSheetArea(format) {
+    // Example: predefined dimensions for A1 size
+    if (format === 'A1') return 594 * 841;
+    // Add other sizes (A2, A3, etc.) here
+}
